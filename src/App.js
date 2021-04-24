bd5518977dde6e4a3d6db8ee45d80291435c053c
berkayozay <berkayyozayy@gmail.com>
2021-04-24 20:17:11 +0300

    add: header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './components/Header'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Let s build Slack! :)</h2>
+    <div className="app">
+      <Header />
     </div>
   );
 }
