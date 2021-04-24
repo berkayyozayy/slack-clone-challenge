d96e7d2ca0f995cb5dcc95a8870e58b51888d9ca
berkayozay <berkayyozayy@gmail.com>
2021-04-24 22:15:18 +0300

    add: sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import './components/Header'
+
+// Components
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className='app__body'>
+        <Sidebar />
+      </div>
     </div>
   );
 }
