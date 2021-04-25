50ba4f573f56fe232a838d3f3c82d28adc763cc9
berkayozay <berkayyozayy@gmail.com>
2021-04-25 09:20:03 +0300

    get channels from firebase

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Sidebar.css'
 
 // Components
@@ -17,8 +17,23 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import db from '../db/firebase';
 
 function Sidebar() {
+
+    const [channels, setChannels] = useState([])
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot((snapshot) => {
+            setChannels(
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    name: doc.data().name,
+                }))
+            )
+        })
+    }, [])
+
     return (
         <div className='sidebar'>
             <div className='sidebar__header'>
@@ -43,6 +58,12 @@ function Sidebar() {
             <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
             <hr />
             <SidebarOption Icon={AddIcon} title='Add Channel' />
+            {channels.map((channel) => (
+                <SidebarOption
+                    id={channel.id}
+                    title={channel.name}
+                />
+            ))}
         </div>
     )
 }
