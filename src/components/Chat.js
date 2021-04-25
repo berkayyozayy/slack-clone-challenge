98c5252bd47800d34068353e7ee66ae32afeada0
berkayozay <berkayyozayy@gmail.com>
2021-04-25 12:29:18 +0300

    fetch room details from database

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Chat.css'
 import { useParams } from 'react-router-dom'
 
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
+import db from '../db/firebase'
+
 function Chat() {
     const { roomId } = useParams()
+    const [roomDetails, setRoomDetails] = useState(null)
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms')
+                .doc(roomId)
+                .onSnapshot((snapshot) => {
+                    setRoomDetails(snapshot.data())
+                })
+        }
+    }, [roomId])
+
     return (
         <div className='chat'>
             <h2>You are in {roomId} room</h2>
             <div className='chat__header'>
                 <div className='chat__headerLeft'>
                     <div className='chat__channelName'>
-                        <strong># general</strong>
+                        <strong>#{roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon />
                     </div>
                 </div>
